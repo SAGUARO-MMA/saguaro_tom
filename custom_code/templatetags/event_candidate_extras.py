4d498096e098d5a05d3695b065fb248f3fbeaeee
Noah Franz <noahfranz13@gmail.com>
2025-12-03 16:39:42 -0700

    update the table sorting to handle the new method for calculating the score

--- custom_code/templatetags/event_candidate_extras.py
+++ custom_code/templatetags/event_candidate_extras.py
@@ -18,47 +18,58 @@ from candidate_vetting.vet_phot import _get_post_disc_phot
 register = template.Library()
 
 @register.simple_tag
-def get_event_candidate_score(event_candidate, *subscore_names):
-    """Get the event candidate score for everything in subscore_names""" 
+def get_event_candidate_scores(event_candidates, *subscore_names):
+    """Get the event candidate scores for everything in subscore_names
 
-    # first get all the subscores for this object
-    subscores = ScoreFactor.objects.filter(
-        event_candidate = event_candidate,
-        key__in = subscore_names
-    ).annotate(
-        value_float = Cast("value", FloatField())
-    )
-    
-    # some of the keys in ScoreFactor are really just calculated values
-    # where the score depends on the type of non-localized event. So we need to convert
-    # these to scores.
-    # I'm writing this just for KN for now, but we can modify as needed!
-    val_not_score_keys = ["phot_peak_lum", "phot_peak_time", "phot_decay_rate"]
-    phot_score = 1
-    if any(_key in subscore_names for _key in val_not_score_keys):
-        # then we need to compute the photometry score and store it
-        allphot = _get_post_disc_phot(
-            target_id = event_candidate.target.id,
-            nonlocalized_event = event_candidate.nonlocalizedevent
-        )
-        phot_score, _, _, _, _, _ = kn_score_phot(
-            allphot,
-            target = event_candidate.target,
-            nonlocalized_event = event_candidate.nonlocalizedevent,
-            filt = ["g", "r", "i", "o", "c"] # use the common optical filters
+    event_candidates should be a django queryset of EventCandidate objects
+    """ 
+
+    ecs_out = []
+    for ec in event_candidates:
+
+        # first get all the subscores for this object
+        subscores = ScoreFactor.objects.filter(
+            event_candidate = ec,
+            key__in = subscore_names
+        ).annotate(
+            value_float = Cast("value", FloatField())
         )
 
-        subscores.exclude(
+        
+        # some of the keys in ScoreFactor are really just calculated values
+        # where the score depends on the type of non-localized event. So we need to convert
+        # these to scores.
+        # I'm writing this just for KN for now, but we can modify as needed!
+        val_not_score_keys = ["phot_peak_lum", "phot_peak_time", "phot_decay_rate"]
+        phot_score = 1
+        if any(_key in subscore_names for _key in val_not_score_keys):
+            # then we need to compute the photometry score and store it
+            allphot = _get_post_disc_phot(
+                target_id = ec.target.id,
+                nonlocalized_event = ec.nonlocalizedevent
+            )
+            phot_score, _, _, _, _, _ = kn_score_phot(
+                allphot,
+                target = ec.target,
+                nonlocalized_event = ec.nonlocalizedevent,
+                filt = ["g", "r", "i", "o", "c"] # use the common optical filters
+            )
+
+        subscores = subscores.exclude(
             key__in = val_not_score_keys
         ) # this removes those rows from the queryset
         
-    # now we can compute the score just using multiplication
-    subscore_list = list(
-        subscores.values_list("value_float", flat=True)
-    )
-    subscore_list.append(phot_score)
-    score = math.prod(subscore_list) # multiply the subscores
-    return score
+        # now we can compute the score just using multiplication
+        subscore_list = list(
+            subscores.values_list("value_float", flat=True)
+        )
+        subscore_list.append(phot_score)
+
+        # save the score to a temporary field in the EventCandidate object
+        ec.score = math.prod(subscore_list) # multiply the subscores
+        ecs_out.append(ec)
+        
+    return sorted(ecs_out, reverse=True, key = lambda x : x.score)
     
 
 #@register.inclusion_tag('tom_targets/partials/target_data.html', takes_context=True)
