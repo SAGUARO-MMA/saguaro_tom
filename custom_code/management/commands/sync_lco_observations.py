e942461d7e520da8ec2d9821ca54e00151865754
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-01-12 08:43:01 -0800

    add default for sync_lco_observations --created-after

--- custom_code/management/commands/sync_lco_observations.py
+++ custom_code/management/commands/sync_lco_observations.py
@@ -3,6 +3,7 @@ from tom_observations.models import ObservationRecord
 from tom_observations.facilities import lco, ocs
 from tom_targets.models import get_target_model_class
 from tom_targets.utils import cone_search_filter
+from datetime import datetime, timedelta
 from urllib.parse import urlencode, urljoin
 import logging
 
@@ -16,13 +17,15 @@ class Command(BaseCommand):
     help = 'Records all available LCO observation requests in our TOM'
 
     def add_arguments(self, parser):
-        parser.add_argument('--created-after', help='Sync observation requests created after this date')
+        parser.add_argument('--created-after', help='Sync observation requests created after this date (default 1 hour ago)')
         parser.add_argument('--created-before', help='Sync observation requests created before this date')
 
     def handle(self, *args, **options):
         query_options = {}
         if options['created_after']:
             query_options['created_after'] = options['created_after']
+        else:
+            query_options['created_after'] = (datetime.now() - timedelta(hours=1.)).strftime('%Y-%m-%dT%H:%M:00Z')
         if options['created_before']:
             query_options['created_before'] = options['created_before']
         query_params = urlencode(query_options)
