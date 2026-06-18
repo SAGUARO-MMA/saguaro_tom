0983e72e160a0e820484962fbbbc19de66a9e8a5
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-06-18 15:58:31 -0500

    move navbar customizations to new integration point

--- tom_surveys/apps.py
+++ tom_surveys/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.urls import path, include
 
 
 class TomSurveysConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tom_surveys"
+
+    def nav_items(self):
+        return [{'partial': f'{self.name}/partials/navbar_surveys.html', 'position': 'left'}]
+
+    def include_url_paths(self):
+        return [path('surveys/', include(f'{self.name}.urls', namespace='surveys'))]
