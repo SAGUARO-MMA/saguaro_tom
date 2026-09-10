a817bd012ef43407f73b4e2ebfec238ad4b88b05
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-09-10 13:36:11 -0700

    reactivate hopskotch stream

--- saguaro_tom/settings.py
+++ saguaro_tom/settings.py
@@ -372,7 +372,7 @@ REST_FRAMEWORK = {
 
 ALERT_STREAMS = [
     {
-        'ACTIVE': False,
+        'ACTIVE': True,
         'NAME': 'tom_alertstreams.alertstreams.hopskotch.HopskotchAlertStream',
         'OPTIONS': {
             'URL': 'kafka://kafka.scimma.org/',
